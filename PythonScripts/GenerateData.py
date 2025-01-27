956e07e5d68463af00bde078523626aa11bcbc7f
wrtabb <wrtabb@gmail.com>
2025-01-27 15:19:57 -0700

    updated script to generate simulated data

--- PythonScripts/GenerateData.py
+++ PythonScripts/GenerateData.py
@@ -1,25 +1,56 @@
 
-from numpy import sin
-from numpy import arange
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-import random
 
+# The true function for the data
 def function_true(x):
-    return (sin(x) + sin((10.0/3.0)*x))**2
+    return (np.sin(x) + np.sin((10.0/3.0)*x))**2
 
-def generate_data():
+# Generate random data using function_true() as truth
+# With gaussian uncertainty on each point
+def generate_data(num_entries):
+    print(f"Generating data for {num_entries} entries")
+
     xmin, xmax = 0.0, 3.5
-    print(f"Setting xmin to {xmin}; setting xmax to {xmax}")
-
-    inputs = arange(xmin,xmax,0.01)
+    inputs = np.arange(xmin,xmax,0.01)
     results = function_true(inputs)
     plt.plot(inputs,results)
-    save_name_true = "true_fuction.png"
-    plt.savefig(save_name_true)
-    print(f"Saved function plot as {save_name_true}")
+    plot_name_true = "../Plots/true_fuction.png"
+    plt.savefig(plot_name_true)
+    print(f"Saved true function plot as {plot_name_true}")
 
-    num = random.uniform(xmin,xmax)
-    print("Random number =",num)
-    return num
+    X = np.full((num_entries),-1000.0)
+    Y = np.full((num_entries),-1000.0)
+    Y_estimate = np.full((num_entries),-1000.0)
 
-generate_data()
+    for i in range(num_entries):
+        X[i] = np.random.uniform(xmin,xmax)
+        Y[i] = function_true(X[i])
+        y_err = np.random.normal(loc=0,scale=0.5)
+        Y_estimate[i] = Y[i]+y_err
+
+    plt.scatter(X,Y_estimate)
+    plot_name_data = f"../Plots/data_plot_{num_entries}.png"
+    plt.savefig(plot_name_data)
+    plt.clf()
+    print(f"Saved data plot as {plot_name_data}")
+
+    df_x = pd.DataFrame(X,columns=['X'])
+    df_y = pd.DataFrame(Y)
+    df_yestimate = pd.DataFrame(Y_estimate)
+
+    df = df_x
+    df.insert(1,"estimate",df_yestimate)
+    df.insert(2,"truth",df_y)
+
+    data_save_name = f"../SavedData/simulated_data_{num_entries}.pkl"
+    df.to_pickle(data_save_name)
+    print(f"Saved dataframe to pickle file {data_save_name}")
+    print("\n")
+
+generate_data(100)
+generate_data(1000)
+generate_data(10000)
+generate_data(100000)
+generate_data(1000000)
